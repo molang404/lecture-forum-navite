20a5bfb15bb1ee08d42b4c7181a2ae149f9acf24
molang404 <art104233@naver.com>
2026-06-19 10:52:34 +0900

    초기화, nativewind 적용, useAuthStore, useThemeStore 준비 완료

---
+++ stores/auth/useAuthStore.ts
@@ -0,0 +1,33 @@
+import { User } from "../../types/user";
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Platform } from "react-native";
+
+type AuthState = {
+    isLoggedIn: boolean;
+    token: string | null;
+    user: User | null;
+
+    login: (user: User, token: string) => void;
+    logout: VoidFunction;
+};
+
+// 이 프로그램이 구동되는 환경에 따라 사용해야 하는 스토리지(저장소)가 달라져야 함
+const storage =
+    Platform.OS === "web"
+        ? createJSONStorage(() => localStorage)
+        : createJSONStorage(() => AsyncStorage);
+
+export const useAuthStore = create<AuthState>()(
+    persist(
+        set => ({
+            isLoggedIn: false,
+            token: null,
+            user: null,
+            login: (user, token) => set({ isLoggedIn: true, token, user }),
+            logout: () => set({ isLoggedIn: false, token: null, user: null }),
+        }),
+        { name: "auth-storage", storage },
+    ),
+);
