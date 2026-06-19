1eb1c26baab214736c6f573d90c2d47e2060209c
molang404 <art104233@naver.com>
2026-06-19 11:37:43 +0900

    테마 적용 완료

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,22 @@
-// /app/_layout.tsx
 import "../styles/global.css";
 import { Slot } from "expo-router";
+import { useThemeStore } from "../stores/theme/useThemeStore";
+import { useColorScheme } from "nativewind";
+import { View } from "react-native";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+    const { theme } = useThemeStore();
+    // 앱에서 라이트모드와 다크모드를 적용하기 위한 기능을 호출
+    const { setColorScheme } = useColorScheme();
+
+    useEffect(() => {
+        setColorScheme(theme);
+    }, [theme, setColorScheme]);
+
     return (
-        <Slot/>
+        <View className={theme}>
+            <Slot />
+        </View>
     );
 }
