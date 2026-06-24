5d5e77f38fd458551596197b34b1c2b02736918a
molang404 <art104233@naver.com>
2026-06-24 16:44:46 +0900

    AdminCategory 완료

--- api/admin/adminCategoryApi.ts
+++ api/admin/adminCategoryApi.ts
@@ -7,10 +7,20 @@ const getCategoryList = async (): Promise<Category[]> => {
     return response.data.data;
 };
 
+const getCategoryById = async (id: number): Promise<Category> => {
+    const response = await axiosInstance.get(`/admin/category/${id}`);
+    return response.data.data;
+};
+
 const createCategory = async (input: AdminCategoryInputType): Promise<Category> => {
     const response = await axiosInstance.post("/admin/category/create", input);
     return response.data.data;
 };
+
+const updateCategory = async (id: number, input: AdminCategoryInputType): Promise<Category> => {
+    const response = await axiosInstance.patch(`/admin/category/${id}`, input);
+    return response.data.data;
+}
 
 const toggleCategoryStatus = async (id: number): Promise<Category> => {
     const response = await axiosInstance.patch(`/admin/category/${id}/status`);
@@ -19,6 +29,8 @@ const toggleCategoryStatus = async (id: number): Promise<Category> => {
 
 export default {
     getCategoryList,
+    getCategoryById,
     createCategory,
+    updateCategory,
     toggleCategoryStatus,
 };
