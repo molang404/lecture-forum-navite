7cdcf80513e284c2ae029ec943b23a6d88578441
molang404 <art104233@naver.com>
2026-06-24 11:04:55 +0900

    AdminAsideMobile 작성 완료

---
+++ app/(admin)/admin/categories/index.tsx
@@ -0,0 +1,7 @@
+import TextComponent from "../../../../components/common/text/TextComponent";
+
+function CategoryListPage() {
+    return <TextComponent>category</TextComponent>;
+}
+
+export default CategoryListPage;
