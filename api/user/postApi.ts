f9ce8f8e0688a3268bc0d172b7ad6f3775891119
molang404 <art104233@naver.com>
2026-07-01 13:29:15 +0900

    PostList 작성 완료

---
+++ api/user/postApi.ts
@@ -0,0 +1,19 @@
+import axiosInstance from "../axiosInstance";
+import { PaginationResponseType } from "../../types/common";
+import { PostListItemType } from "../../types/post";
+
+const getPostsByCategory = async (
+    categoryId: number,
+    page: number,
+    size: number,
+): Promise<PaginationResponseType<PostListItemType>> => {
+    const response = await axiosInstance.get(`/post/list/${categoryId}`, {
+        params: {
+            page,
+            size,
+        },
+    });
+    return response.data.data;
+};
+
+export default { getPostsByCategory };
